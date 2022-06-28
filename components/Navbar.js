27a0c9aeb2cea21c5704801ac0b5d884e057aab3
James <1536755789@qq.com>
2022-06-28 14:49:32 +0800

    second

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,20 +1,26 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import {useContext} from "react";
+import AuthContext from "../stores/authContext";
 
 export default function Navbar() {
-  return (
-    <div className="container">
-      <nav>
-        <Image src="/rupee.png" width={50} height={48} />
-        <h1>HuiHui DAPP</h1>
-        <ul>
-          <li><Link href="/"><a>主页</a></Link></li>
-          <li><Link href="/guides"><a>付费解锁内容</a></Link></li>
-        </ul>
-      </nav>
-      <div className="banner">
-        <Image src="/banner.png" width={966} height={276} />
-      </div>
-    </div>
-  )
+    const { user, login, logout } = useContext(AuthContext)
+    console.log(user)
+    return (
+        <div className="container">
+            <nav>
+                <Image src="/rupee.png" width={50} height={48}/>
+                <h1>HuiHui DAPP</h1>
+                <ul>
+                    <li><Link href="/"><a>主页</a></Link></li>
+                    <li><Link href="/guides"><a>付费解锁内容</a></Link></li>
+                    <li onClick={login} className="btn">登录/注册</li>
+                    <li onClick={logout} className="btn">登出</li>
+                </ul>
+            </nav>
+            <div className="banner">
+                <Image src="/banner.png" width={966} height={276}/>
+            </div>
+        </div>
+    )
 }
--- stores/authContext.js
+++ stores/authContext.js
@@ -1,10 +1,56 @@
-import { createContext} from "react";
+import {createContext, useEffect, useState} from "react";
+import netlifyIdentity from 'netlify-identity-widget'
 
 const AuthContext = createContext({
     user: null,
     login: () => {
 
     },
-    logout: () => {},
+    logout: () => {
+
+    },
     authReady: false
 })
+
+export const AuthContextProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    useEffect( () => {
+        netlifyIdentity.on('login', (user) => {
+            setUser(user)
+            netlifyIdentity.close();
+            console.log('login event')
+        });
+
+        netlifyIdentity.on('logout', () => {
+            setUser(null);
+            console.log('logout user')
+        })
+
+        // init netlify identity connection
+        netlifyIdentity.init()
+
+        return () => {
+            netlifyIdentity.off('login')
+            netlifyIdentity.off('logout')
+        }
+
+
+    }, []);
+    const login = () => {
+        netlifyIdentity.open()
+    }
+
+
+    const logout = () => {
+        netlifyIdentity.logout()
+    }
+
+    const context = { user, login, logout }
+    return (
+        <AuthContext.Provider value={context}>
+            { children }
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthContext
